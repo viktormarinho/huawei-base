cdd61556d8513befb57361928174b7d7936e887d
vinicius-prates <viniprates2211@gmail.com>
2022-10-19 08:02:56 -0300

    created the frontend using react + vite + ts, added tailwind to the project as well

---
+++ frontend/huawei-base-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-lg text-blue-600">blau</h1>
+    </div>
+  )
+}
+
+export default App
