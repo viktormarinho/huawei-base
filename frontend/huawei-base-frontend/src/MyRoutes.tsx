ddae27022afaa31b45bfc097a7dde71ea637d5b8
vinicius-prates <viniprates2211@gmail.com>
2022-10-19 08:24:07 -0300

    added routes, and added DataBaseTables page

---
+++ frontend/huawei-base-frontend/src/MyRoutes.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import App from "./App"
+import { DataBaseTables } from "./Pages/DataBaseTables"
+export const MyRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="/panel/tables" element={<DataBaseTables/>}/>
+                        
+        </Routes>
+        
+
+    )
+}
---
+++ frontend/huawei-base-frontend/src/Pages/DataBaseTables.tsx
@@ -0,0 +1,6 @@
+export const DataBaseTables = () => {
+
+    return(
+        <h1>começouuuuuu</h1>
+    )
+}
