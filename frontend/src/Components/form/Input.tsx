653859dfb8baf23016fc7b6f7e5ae3947548a29b
iguoliveira <igor.or042@gmail.com>
2022-10-20 15:17:13 -0300

    👌 IMPROVE: Animations in all buttons

--- frontend/src/Components/form/Input.tsx
+++ frontend/src/Components/form/Input.tsx
@@ -10,6 +10,6 @@ interface InputProps {
 
 export const Input = ({ type, placeholder, value, onChange, id, name, isError }: InputProps) => {
     return (
-        <input id={id} type={type} value={value} onChange={onChange} name={name} placeholder={placeholder} className={`bg-[#F2F2F2] w-[30rem] p-4 rounded-lg text-xl border-red-500 focus:outline-none ${isError ? ('border-2') : ('focus:rounded-l-none focus:border-l-4')}`} />
+        <input id={id} type={type} value={value} onChange={onChange} name={name} placeholder={placeholder} className={`bg-[#F2F2F2] w-[30rem] p-4 rounded-lg text-xl border-red-500 focus:outline-none hover:bg-[#e9e9e9] ${isError ? ('border-2') : ('focus:rounded-l-none focus:border-l-4')}`} />
     )
 }
