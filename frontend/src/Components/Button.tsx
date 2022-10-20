d77e3ac8fc2729f7d66fb40ab12f4dd697acc8bf
vinicius-prates <viniprates2211@gmail.com>
2022-10-20 16:07:58 -0300

    fix button style

--- frontend/src/Components/Button.tsx
+++ frontend/src/Components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ text, isDefault, isAdmin, onClick }: ButtonProps) => {
     return (
         <button onClick={onClick} 
         className={`{${isAdmin ? (`bg-[{${isDefault ? '#242424' : 'whitesmoke'}}] text-[#242424] text-xl rounded-lg px-2 h-[2.4rem] border-2 border-gray-900 w-40 hover:bg-[#242424] hover:text-white transition-all `) :
-         ("text-2xl font-bold text-white   select-none button-gradient-animation")}`}>
+         ("text-2xl font-bold text-white select-none button-gradient-animation rounded-2xl p-4")}`}>
             {text}
         </button>
     )
