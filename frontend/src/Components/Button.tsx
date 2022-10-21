7db3818f4c00cf2f9a3e8f1737bb5a975e5f82b3
vinicius-prates <viniprates2211@gmail.com>
2022-10-21 14:53:59 -0300

    Responsivity added to landing page

--- frontend/src/Components/Button.tsx
+++ frontend/src/Components/Button.tsx
@@ -1,16 +1,35 @@
 interface ButtonProps {
-    text: string
-    isDefault?: boolean
-    isAdmin: boolean
-    onClick?: any
+  text: string;
+  isDefault?: boolean;
+  isAdmin: boolean;
+  onClick?: any;
+  isMobile?: boolean;
 }
 
-export const Button = ({ text, isDefault, isAdmin, onClick }: ButtonProps) => {
-    return (
-        <button onClick={onClick} 
-        className={`{${isAdmin ? (`bg-[{${isDefault ? '#242424' : 'whitesmoke'}}] text-[#242424] text-xl rounded-lg px-2 h-[2.4rem] border-2 border-gray-900 w-40 hover:bg-[#242424] hover:text-white transition-all `) :
-         ("text-2xl font-bold text-white select-none button-gradient-animation rounded-2xl p-4")}`}>
-            {text}
-        </button>
-    )
-}
+export const Button = ({
+  text,
+  isDefault,
+  isAdmin,
+  onClick,
+  isMobile = false,
+}: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${
+        isAdmin
+          ? `bg-[${
+              isDefault ? "#242424" : "whitesmoke"
+            }] text-[#242424] text-xl rounded-lg px-2 h-[2.4rem] border-2 border-gray-900 w-40 hover:bg-[#242424] hover:text-white transition-all `
+          : `text-2xl font-bold text-white select-none rounded-2xl p-4 ${
+              isMobile
+                ? "bg-gradient-to-r from-gray-400 to-gray-800"
+                : "button-gradient-animation"
+            }`
+      }`}
+      disabled={isMobile && true}
+    >
+      {text}
+    </button>
+  );
+};
--- frontend/src/Components/landingPageComponents/LandingForm.tsx
+++ frontend/src/Components/landingPageComponents/LandingForm.tsx
@@ -22,10 +22,10 @@ export const LandingForm = () => {
     }
 
     return (
-        <form className="flex flex-col gap-6">
+        <form className="flex flex-col gap-6 justify-center items-center">
             <Input id={"email"} type={"email"} name={"email"} placeholder={"Email"} value={input.email} onChange={(evt: any) => handleChange(evt)} />
             <Input id={"subject"} type={"text"} name={"subject"} placeholder={"Subject"} value={input.subject} onChange={(evt: any) => handleChange(evt)} />
-            <textarea id="message" name="message" placeholder="Message here" value={input.message} onChange={(evt: any) => handleChange(evt)} className="bg-[#F2F2F2] h-[16rem] resize-none p-4 rounded-lg text-xl border-red-500 hover:bg-[#e9e9e9] focus:outline-none focus:rounded-l-none focus:border-l-4" />
+            <textarea id="message" name="message" placeholder="Message here" value={input.message} onChange={(evt: any) => handleChange(evt)} className="bg-[#F2F2F2] h-[16rem] w-[20rem] md:w-[30rem] resize-none p-4 rounded-lg text-xl border-red-500 hover:bg-[#e9e9e9] focus:outline-none focus:rounded-l-none focus:border-l-4" />
             <Button onClick={(evt: any) => handleForm(evt)} text={"SEND"} isAdmin={false} />
         </form>
     )
